f4c5524d85e5acde0c228a8a1b0b3b22dc722390
tcxcx <72812526+tcxcx@users.noreply.github.com>
2021-12-13 22:46:03 -0500

    Add files via upload

---
+++ js_tarea/consultar_saldo.js
@@ -0,0 +1,13 @@
+function bienvenido () {
+    var nombres = Array[[Mali, Gera, Maui]]
+
+}
+
+[Mali, Gera, Maui]
+
+if (condition) {
+    
+} else {
+    
+}
+//idea a desarrollar: array [Mali, Gera, Maui] hacer relacion if - Mali ingresa a su portal - innerHTML.Mali
